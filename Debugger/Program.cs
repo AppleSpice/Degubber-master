cd4f8c1311edc3c0649593ad963e209759e83637
AppleSpice <58701821+AppleSpice@users.noreply.github.com>
2020-09-13 13:32:33 -0400

    Changed name

--- Debugger/Program.cs
+++ Debugger/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Degubber // <-- fix this so that it says Debugger
+namespace Debugger // <-- fix this so that it says Debugger
 {
     class Program
     {
--- Debugger/Properties/AssemblyInfo.cs
+++ Debugger/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Degubber")]
+[assembly: AssemblyTitle("Debugger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Degubber")]
+[assembly: AssemblyProduct("Debugger")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
